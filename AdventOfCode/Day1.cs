6ee32a29f99d08047ab56f817546c19b2466435b
Darius Vollmer <dariusv@outlook.de>
2022-12-15 19:14:03 +0100

    git init and first day

---
+++ AdventOfCode/Day1.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+
+namespace AdventOfCode
+{
+    class Day1
+    {
+        static void Main()
+        {
+            CalorieCounting();
+        }
+        public static void CalorieCounting()
+        {
+            string s = File.ReadAllText(@"C:/Users/Dariu/Documents/source/AdventOfCode/AdventOfCode/CalorieCounting.txt");
+            Console.WriteLine(s);
+        }   
+    }
+}

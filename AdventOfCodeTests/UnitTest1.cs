f8c04cc6259d7fc0bdf3febe26e38f2634a6d2fe
Darius Vollmer <dariusv@outlook.de>
2022-12-16 11:15:35 +0100

    added testcases for Day2 and made references to the test suite

--- AdventOfCodeTests/UnitTest1.cs
+++ AdventOfCodeTests/UnitTest1.cs
@@ -1,9 +1,16 @@
+using AdventOfCode;
+
 namespace AdventOfCodeTests;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void Day2()
     {
+        Dictionary<int, string> battles = new Dictionary<int, string>();
+        battles.Add(0, "AY");
+        battles.Add(1, "BX");
+        battles.Add(2, "CZ");
+        Assert.Equal(15, AdventOfCode.Day2.CalcScore(battles));
     }
 }

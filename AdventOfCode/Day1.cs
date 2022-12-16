ef49b2eb722775967cc2a6cf49a5797c2d855293
Darius Vollmer <dariusv@outlook.de>
2022-12-16 07:56:50 +0100

    Day 1.1 working and 1.2 prototype

--- AdventOfCode/Day1.cs
+++ AdventOfCode/Day1.cs
@@ -8,10 +8,42 @@ namespace AdventOfCode
         {
             CalorieCounting();
         }
-        public static void CalorieCounting()
+
+        private static void CalorieCounting()
         {
-            string s = File.ReadAllText(@"C:/Users/Dariu/Documents/source/AdventOfCode/AdventOfCode/CalorieCounting.txt");
-            Console.WriteLine(s);
-        }   
+            Dictionary<int, int> elves = new Dictionary<int, int>();
+            elves.Add(0, 0);
+            int index = 0;
+            foreach (string line in File.ReadLines(
+                         @"C:/Users/Dariu/Documents/source/AdventOfCode/AdventOfCode/CalorieCounting.txt"))
+            {
+                if (String.IsNullOrEmpty(line))
+                {
+                    index++;
+                    elves.Add(index, 0);
+                    continue;
+                }
+
+                elves[index] += Convert.ToInt32(line);
+            }
+            GetTopElves(elves, 1);
+            GetTopElves(elves, 3);
+        }
+
+        private static void GetTopElves(Dictionary<int, int> elves, int topCount)
+        {
+            if (topCount == 1)
+            { 
+                Console.WriteLine(
+                    $"The elf with the highest calorie amount is elf {elves.Keys.Max()} with a count of {elves.Values.Max()} \n");
+            }
+            
+            for (int i = 1; i < topCount + 1; i++)
+            {
+                Console.WriteLine(
+                    $"The elf with the {i}. calorie amount is elf {elves.Keys.Max()} with a count of {elves.Values.Max()} \n");
+                elves.Remove(elves.Keys.Max());
+            }
+        }
     }
 }
